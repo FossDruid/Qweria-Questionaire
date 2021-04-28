e6c42b23bf1d07135ec8d71bed05581de0f14cb7
Mattei <35488936+LunchTimeDoublySo@users.noreply.github.com>
2021-04-29 01:03:45 +0200

    Create header.php
    Header for SPA

---
+++ header.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+
+ <!--  header -->
+ <html>	
+<style>
+.header{
+	position: relative;
+	overflow: hidden;
+	background-color: #4ba3c7; 
+	padding: 20px 10px;
+ }
+
+</style>
+	 <body>
+		<div class="header">
+		<h1><?php echo $pageTitle?></h1>
+		<h4><?php echo $siteEng?></h4>
+		<p><?php echo $siteVersion?></p>
+		</div>
+		<hr>
+	 </body>
+ </html> 
